2a9b470380464eaa4845effd34c9e9f49072ee7f
Dipti R <razdan.dipti@gmail.com>
2021-07-16 15:20:15 -0400

    updated the folders and deleted unwanted files

---
+++ portfolio-dr/src/components/topbar/Topbar.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import'./topbar.scss'
+import {Person, Mail} from '@material-ui/icons' 
+
+export default function Topbar(){
+    return(
+        <div className='topbar'>
+            <div className='wrapper'>
+                <div className='left'>
+                    <a href='#intro' className='logo'>DR</a> 
+                    <div className='itemContainer'>
+                        <Person className='icon' />
+                        <span>+89765</span>
+                    </div>
+                    <div className='itemContainer'>
+                        <Mail className='icon'/>
+                        <span>d.r@gmail.com</span>
+                    </div>
+                </div>
+                
+
+                <div className='right'></div>
+                
+
+
+            </div>
+
+            
+        </div>
+    )
+}
